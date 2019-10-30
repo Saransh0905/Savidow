8b84e34bd6676dd90f2264e80dcc193c52e47472
Saransh <b18191@students.iitmandi.ac.in>
2019-10-30 18:42:55 +0530

    version 2.0

--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
 
 document.addEventListener('DOMContentLoaded',function(){
+    var sel = document.getElementById('listOfKeys');
+    for (let i = 0; i < localStorage.length; i++) {
+        var opt = document.createElement('option');
+        opt.appendChild( document.createTextNode(localStorage.key(i)));
+        opt.value = localStorage.key(i) ; 
+        sel.appendChild(opt); 
+        
+    }
     
     var MemoriseButton = document.getElementById('memorise_button');
     var RetrieveButton = document.getElementById('retrieve_button');
@@ -8,22 +16,20 @@ document.addEventListener('DOMContentLoaded',function(){
         var sessionName = document.getElementById('sessionName').value;
         alert("You have saved all tabs");
         var urlArray = [];
-        chrome.tabs.getAllInWindow(null,function(tabs)
-        {
-        tabs.forEach(element => 
-            {
+        chrome.tabs.getAllInWindow(null,function(tabs){
+        tabs.forEach(element =>{
             urlArray.push(element.url)
         })
-        alert(urlArray);
         localStorage.setItem(sessionName,urlArray);
         console.log(localStorage.getItem("hello"));
+    });
 
-
-    }); 
     },false);
     
     RetrieveButton.addEventListener('click',function(){
-        var existingName = document.getElementById('existingName').value;
-        console.log(localStorage.getItem(existingName));
+        var toOpen = document.getElementById("listOfKeys").value;
+        var urlArray = localStorage.getItem(toOpen).split(",");
+        //console.log(localStorage.getItem(toOpen));
+        chrome.windows.create({url: urlArray});
     },false);
 },false);
